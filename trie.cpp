b1453c607578ceea705117edaf6994aa33809040
Jameer Babu <jameer.jb@gmail.com>
2018-03-20 07:48:09 -0700

    Add files via upload

--- trie.cpp
+++ trie.cpp
@@ -1,91 +1,80 @@
-// C++ implementation of search and insert
-// operations on Trie
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
- 
-const int ALPHABET_SIZE = 26;
- 
-// trie node
-struct TrieNode
+
+typedef long long int ll;
+
+const int MaxN = 500500;
+void insert(string s,bool created[],ll end[],ll next[][MaxN],ll sz)
 {
-    struct TrieNode *children[ALPHABET_SIZE];
- 
-    // isEndOfWord is true if the node represents
-    // end of a word
-    bool isEndOfWord;
-};
- 
-// Returns new trie node (initialized to NULLs)
-struct TrieNode *getNode(void)
-{
-    struct TrieNode *pNode =  new TrieNode;
- 
-    pNode->isEndOfWord = false;
- 
-    for (int i = 0; i < ALPHABET_SIZE; i++)
-        pNode->children[i] = NULL;
- 
-    return pNode;
+	int v = 0;
+    
+	for (int i = 0; i < s.size(); ++i) {
+		int c = s[i] - 'a';
+		if (!created[next[c][v]]) {
+			next[c][v] = ++sz;
+			created[sz] = true;
+		}
+		v = next[c][v];
+	}
+	++end[v];
 }
- 
-// If not present, inserts key into trie
-// If the key is prefix of trie node, just
-// marks leaf node
-void insert(struct TrieNode *root, string key)
+
+bool search (string tmp,bool created[],ll end[],ll next[][MaxN]) 
 {
-    struct TrieNode *pCrawl = root;
- 
-    for (int i = 0; i < key.length(); i++)
-    {
-        int index = key[i] - 'a';
-        if (!pCrawl->children[index])
-            pCrawl->children[index] = getNode();
- 
-        pCrawl = pCrawl->children[index];
-    }
- 
-    // mark last node as leaf
-    pCrawl->isEndOfWord = true;
+	int v = 0;
+	
+	for (int i = 0; i < tmp.size(); ++i) {
+		int c = tmp[i] - 'a';
+		if (!created[next[c][v]])
+			return false;
+		v = next[c][v];
+	}
+	return end[v] > 0;
 }
- 
-// Returns true if key presents in trie, else
-// false
-bool search(struct TrieNode *root, string key)
-{
-    struct TrieNode *pCrawl = root;
- 
-    for (int i = 0; i < key.length(); i++)
-    {
-        int index = key[i] - 'a';
-        if (!pCrawl->children[index])
-            return false;
- 
-        pCrawl = pCrawl->children[index];
-    }
- 
-    return (pCrawl != NULL && pCrawl->isEndOfWord);
-}
- 
-// Driver
 int main()
 {
-    // Input keys (use only 'a' through 'z'
-    // and lower case)
-    string keys[] = {"the", "a", "there",
-                    "answer", "any", "by",
-                     "bye", "their" };
-    int n = sizeof(keys)/sizeof(keys[0]);
- 
-    struct TrieNode *root = getNode();
- 
-    // Construct trie
-    for (int i = 0; i < n; i++)
-        insert(root, keys[i]);
- 
-    // Search for different keys
-    search(root, "the")? cout << "Yes\n" :
-                         cout << "No\n";
-    search(root, "these")? cout << "Yes\n" :
-                           cout << "No\n";
-    return 0;
+	ll t;
+	cin >> t;
+	while(t--)
+	{
+		
+		ll sz = 0;
+		
+		ll next[27][MaxN];
+		ll end[MaxN];
+		bool created[MaxN];
+		
+		ll n,q;
+		cin >> n >> q;
+		vector<string>str;
+		
+		for(ll i=0;i<n;i++)
+		{
+			string s;
+			cin >> s;
+			insert(s,created,end,next,sz);
+			sz=sz+s.size();
+		}
+		
+		
+		// cout << sz << endl;
+		
+		
+		for(ll i=0;i<q;i++)
+		{
+			string s;
+			cin >> s;
+			
+			if(search(s,created,end,next))
+			{
+				cout << "yes" << endl;
+			}
+			else
+			{
+				cout << "no" << endl;
+			}
+		}
+	}
+	
+	return 0;
 }
